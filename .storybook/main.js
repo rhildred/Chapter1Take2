2d1220998f2b8518c5f7dde05afb729d0cc3591e
rhildred <rhildred@gmail.com>
2023-08-03 11:25:21 -0400

    boost to newest version diy-pwa

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+import "w3-css/w3.css";
 const config = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
